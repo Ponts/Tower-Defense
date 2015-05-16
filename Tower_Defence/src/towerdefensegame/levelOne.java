eee748d1bb582afc63eaf10a03b609d7046be807
Leonardo Freccero <leonardofreccero@gmail.com>
2015-05-16 13:47:18 +0200

    more "enemies" began implementing towers

--- Tower_Defence/src/towerdefensegame/levelOne.java
+++ Tower_Defence/src/towerdefensegame/levelOne.java
@@ -13,11 +13,15 @@ import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import towerdefensegame.objects.Tower;
+
 public class levelOne extends BasicGameState {
 	private int ID; 
 	private Image theMap = null;
+	private ArrayList<Tower> towers;
 	//private ArrayList<ArrayList<Circle>> waves;
 	private ArrayList<Circle> waves;
+	
 	private float circleX;
 	private float circleY;
 	
@@ -32,9 +36,15 @@ public class levelOne extends BasicGameState {
 	
 	public void init(GameContainer container, StateBasedGame sbg) throws SlickException {
 		theMap = new Image("res//MAP1.png");
+		
+		towers = new ArrayList<Tower>();
+		
+		
 		waves = new ArrayList<Circle>();
 		waves.add(new Circle(40, 0, 5));
-		baseHealth = 30;
+		waves.add(new Circle(40, -30, 5));	
+		waves.add(new Circle(40, -60, 5));
+		baseHealth = 3;
 	}
 	
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException {
@@ -45,6 +55,9 @@ public class levelOne extends BasicGameState {
 		boolean rightMousePressed = input.isMousePressed(input.MOUSE_RIGHT_BUTTON);
 		boolean leftMousePressed = input.isMousePressed(input.MOUSE_LEFT_BUTTON);
 		
+		if(leftMousePressed){
+			towers.add(new Tower("basic", xPos, yPos));
+		}
 		
 		Circle c;
 		for(int i = waves.size() -1; i >= 0; i-- ){
@@ -88,6 +101,13 @@ public class levelOne extends BasicGameState {
 
 	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.drawImage(theMap, 0, 0);
+		for(Tower t: towers){
+			g.drawImage(t.getSprite(), t.getX(), container.getHeight() - t.getY());
+		}
+		
+		
+		g.setColor(Color.black);
+		g.drawString("remaining life is: " + baseHealth, 5, 5);
 		
 		g.setColor(Color.blue);
 		for(Circle c: waves){
