f7aa3d49ef432ff53b49d13590c70b9059613dcb
Leonardo Freccero <leonardofreccero@gmail.com>
2015-05-18 12:18:34 +0200

    updated enemies

--- Tower_Defence/src/towerdefensegame/objects/Enemy.java
+++ Tower_Defence/src/towerdefensegame/objects/Enemy.java
@@ -1,19 +1,43 @@
 package towerdefensegame.objects;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Enemy {
-	private int walkSpeed;
+	private double walkSpeed;
 	private int attackDamage;
 	private int attackSpeed;
 	private int health;
+	private int bounty;
 	private String special = null;
 	
+	private float X;
+	private float Y;
 	
-	public void initTank(){
-		this.walkSpeed = 1;
-		this.attackDamage = 2;
-		this.attackSpeed = 1;
-		this.health = 500;
-		//this.special = ...
+	private Image sprite;
+	
+	public Enemy(String type, float x, float y) throws SlickException{
+		this.sprite = new Image("res//" + type + ".png");
+		this.X = x;
+		this.Y = y;
+		
+		if(type == "tank"){
+			this.walkSpeed = 0.03;
+			this.attackDamage = 4;
+			this.attackSpeed = 1;
+			this.health = 700;
+			this.bounty = 100;
+			//this.special = ...
+		}
+		else if(type == "small"){
+			this.walkSpeed = 2;
+			this.attackDamage = 1;
+			this.attackSpeed = 2;
+			this.health = 150;
+			this.bounty = 20;
+			//this.special = ...
+		}
+		
 	}
 	
 	private void walk(){
@@ -30,6 +54,36 @@ public class Enemy {
 	
 	private void bounty(){
 		
+	}
+
+	public float getX() {
+		return this.X;
+	}
+
+	public float getY() {
+		return this.Y;
+	}
+
+	public void setY(float f, String s) {
+		if(s == "d"){
+			this.Y += f*this.walkSpeed;
+		}
+		else if(s == "u"){
+			this.Y -= f*this.walkSpeed;
+		}
+	}
+
+	public void setX(float f, String s) {
+		if(s == "r"){
+			this.X += f*this.walkSpeed;
+		}
+		else if(s == "l"){
+			this.X -= f*this.walkSpeed;
+		}
+	}
+
+	public Image getSprite() {
+		return this.sprite;
 	}
 	
 }
